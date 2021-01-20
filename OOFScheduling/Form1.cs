c68f83977985370cf26ce1bbb7081af29d3d6668
EvanBasalik <evanba@microsoft.com>
2021-01-19 21:49:38 -0500

    working through logic reworks for new math

--- OOFScheduling/Form1.cs
+++ OOFScheduling/Form1.cs
@@ -607,7 +607,7 @@ namespace OOFScheduling
             OOFSponder.Logger.Info("previousDayPeriodEnd = " + previousDayPeriodEnd);
 
             DateTime nextDayPeriodStart = OOFData.Instance.nextOOFPeriod.StartTime;
-            OOFSponder.Logger.Info("nextDayPeriodState = " + nextDayPeriodStart);
+            OOFSponder.Logger.Info("nextDayPeriodStart = " + nextDayPeriodStart);
 
             DateTime nextDayPeriodEnd = OOFData.Instance.nextOOFPeriod.EndTime;
             OOFSponder.Logger.Info("nextDayPeriodEnd =" + nextDayPeriodEnd);
@@ -641,8 +641,8 @@ namespace OOFScheduling
                 }
                 else
                 {
-                    StartTime = nextDayPeriodStart;
-                    EndTime = nextDayPeriodEnd;
+                    StartTime = currentWorkingTime.EndTime;
+                    EndTime = nextDayPeriodStart;
                 }
             }
 
